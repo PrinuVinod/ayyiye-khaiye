05462991375218d01fec638336183250b1479756
Prinu Vinod Nair <71308351+PrinuVinod@users.noreply.github.com>
2024-01-16 12:07:04 +0530

    added notification updation

--- routes/kitchenRoutes.js
+++ routes/kitchenRoutes.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const KitchenView = require('../models/KitchenView');
+const Notification = require('../models/Notification');
 
 router.get('/kitchenview', async (req, res) => {
     try {
         const kitchenData = await KitchenView.find().sort({ tableNumber: 1 });
-        res.render('kitchenview', { kitchenData });
+        const notifications = await Notification.find();
+        res.render('kitchenview', { kitchenData, notifications });
     } catch (error) {
         console.error('Error fetching kitchen data:', error);
         res.status(500).send('Internal Server Error');
@@ -24,6 +26,22 @@ router.post('/deleteItem', async (req, res) => {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
+});
+
+router.put('/notifications/:id/status', async (req, res) => {
+    try {
+        const notification = await Notification.findById(req.params.id);
+
+        if (notification) {
+            notification.status = !notification.status;
+            await notification.save();
+            res.json({ success: true, status: notification.status });
+        } else {
+            res.status(404).json({ success: false, message: 'Notification not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 });
 
 module.exports = router;
